d3e5aadbf9fcc43674467703a58fdbed6a36e3b2
PaulBDev <pythonkoder@gmail.com>
2023-03-09 14:55:09 -0600

    heroku

--- backend/index.js
+++ backend/index.js
@@ -45,10 +45,10 @@ app.use("/api", rateLimiter, routes);
 
 // Point the server to the build folder of the app
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "./dist")));
+  app.use(express.static(path.join(__dirname, "../client/dist")));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "./", "dist", "index.html"));
+    res.sendFile(path.resolve(__dirname, "../client", "dist", "index.html"));
   });
 }
 
